f4ab4cac4e7f2613247b5c2ec0b65eb99a1b5dfc
Arpit Maheshwari <arp19690@gmail.com>
2017-01-03 16:38:37 +0530

    adding codebase

---
+++ facebookFunctions.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python
+
+import urllib
+
+import facebook
+import warnings
+import requests
+import os
+
+
+# Hide deprecation warnings. The facebook module isn't that up-to-date (facebook.GraphAPIError).
+warnings.filterwarnings('ignore', category=DeprecationWarning)
+
+
+def get_app_access_token(fb_app_id, fb_app_secret):
+    return facebook.get_app_access_token(fb_app_id, fb_app_secret)
+
+
+def post_message_on_fb(fb_profile_id, oauth_access_token, json_data):
+    facebook_graph = facebook.GraphAPI(oauth_access_token)
+
+    message = json_data["message"]
+    attachments_dict = get_attachments_dict(json_data, oauth_access_token)
+
+    # Try to post something on the wall.
+    try:
+        fb_response = facebook_graph.put_wall_post(message=message,
+                                                   attachment=attachments_dict,
+                                                   profile_id=fb_profile_id)
+        return True, fb_response
+    except facebook.GraphAPIError as e:
+        return False, 'Something went wrong:', e.type, e.message
+
+
+def upload_photo(image_file_path, oauth_access_token):
+    facebook_graph = facebook.GraphAPI(oauth_access_token)
+    fb_response = facebook_graph.put_photo(image=open(image_file_path, 'rb'))
+    return fb_response
+
+
+def download_photo(source, destination):
+    return urllib.urlretrieve(source, destination)
+
+
+def remove_photo(destination):
+    return os.remove(destination)
+
+
+def get_image_url_on_id(photo_id, oauth_access_token):
+    api_url = "https://graph.facebook.com/" + str(photo_id) + "?fields=images&access_token=" + oauth_access_token
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        return response.json()["images"][0]["source"]
+    else:
+        return False
+
+
+def get_timeline_posts(fb_page_id, since_timestamp, oauth_access_token,
+                       get_fields="id,link,picture,source,message,created_time,full_picture,description",
+                       limit=None, data_format="json"):
+    api_url = "https://graph.facebook.com/v2.8/" + fb_page_id + "/feed?fields=" + get_fields + "&since=" + since_timestamp
+    if limit is not None:
+        api_url += "&limit=" + limit
+    api_url += "&format=" + data_format + "&access_token=" + oauth_access_token
+
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        return response.json()["data"]
+    else:
+        return False
+
+
+def get_attachments_dict(json_data, oauth_access_token):
+    attachment_dict = {}
+
+    if "full_picture" in json_data:
+        image_file_path = 'tmpdata/' + str(json_data["id"]) + ".jpg"
+        download_photo(json_data["full_picture"], image_file_path)
+        upload_response = upload_photo(image_file_path, oauth_access_token)
+        remove_photo(image_file_path)
+
+        fb_image_url = get_image_url_on_id(upload_response["id"], oauth_access_token)
+        attachment_dict["picture"] = fb_image_url
+
+        if "source" in json_data:
+            attachment_dict["link"] = json_data["link"]
+        else:
+            attachment_dict["link"] = fb_image_url
+
+    if "message" in json_data:
+        attachment_dict["caption"] = json_data["message"]
+        attachment_dict["name"] = json_data["message"]
+    elif "story" in json_data:
+        attachment_dict["caption"] = json_data["story"]
+        attachment_dict["name"] = json_data["story"]
+
+    if "description" in json_data:
+        attachment_dict["description"] = json_data["description"]
+
+    return attachment_dict
---
+++ run.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+from datetime import datetime, timedelta
+import sys
+
+from facebookFunctions import post_message_on_fb, get_timeline_posts
+from config import access_tokens_list
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+some_timestamp = datetime.now() - timedelta(days=10)
+since_timestamp = str(some_timestamp.strftime('%Y-%m-%dT%H:%M'))
+for data in access_tokens_list:
+    new_posts = get_timeline_posts("ImUrvashiRautela", since_timestamp, data["access_token"])
+    if len(new_posts) > 0:
+        for json_data in new_posts:
+
+            if "message" not in json_data:
+                message = "Urvashi Rautela #UrvashiRautela"
+            else:
+                message = json_data["message"].encode("utf-8")
+                try:
+                    unicode(message, "ascii")
+                except UnicodeError:
+                    message = unicode(message, "utf-8")
+                else:
+                    # value was valid ASCII data
+                    pass
+            json_data.update({"message": message})
+
+            try:
+                api_status, api_message = post_message_on_fb(data["profile_id"], data["access_token"], json_data)
+                if api_status:
+                    print("Message successfully posted on " + data["name"] + "'s Timeline")
+                else:
+                    print(api_message)
+            except Exception as e:
+                print("An error occurred: " + str(e))
+                pass
