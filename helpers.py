718911dfe074ead7e8b61a32f1ba21a69a437686
Arpit Maheshwari <arp19690@gmail.com>
2017-01-24 12:57:40 +0530

    speak the message as welll

--- helpers.py
+++ helpers.py
@@ -2,5 +2,7 @@ import os
 
 
 def mac_notify(title, message):
-    os.system('terminal-notifier -title "' + str(title) + '" -message "' + str(message) + '"')
+    os.system('terminal-notifier -title "' + str(title) + '" -message "' + str(
+        message) + '"')
+    os.system("say Sir, " + str(message))
     return True
