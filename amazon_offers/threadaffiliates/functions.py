f99a3153d6ac84fde9ec22e91162c453c2e7895c
Arpit Maheshwari <arp19690@gmail.com>
2017-03-07 16:46:06 +0530

    bug fix

--- amazon_offers/threadaffiliates/functions.py
+++ amazon_offers/threadaffiliates/functions.py
@@ -44,7 +44,7 @@ def fetch_products(limit="0,5"):
     if len(data) > 0:
         for tmpdata in data:
             post_data_dict = {
-                "name": tmpdata["product_title"].decode("utf-8"),
+                "name": str(tmpdata["product_title"]),
                 "description": "Starts at Rs. " + str(
                     int(tmpdata["product_price_min"])),
                 "picture": tmpdata["product_image_url"],
