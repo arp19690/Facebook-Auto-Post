6434f620003603cf699e68d42d4d2dd6071dd470
Arpit Maheshwari <arp19690@gmail.com>
2017-03-10 13:57:12 +0530

    creating bash script to run different programs

--- run.sh
+++ run.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-echo "Activating VirtualENV"
-. /Applications/XAMPP/htdocs/work/svn/Facebook-Auto-Post/venv/bin/activate
+if [ $1 = "fbautopost" ];then
+    echo "Activating VirtualENV"
+    . /Applications/XAMPP/htdocs/work/svn/Facebook-Auto-Post/venv/bin/activate
 
-echo "Running the script now"
-python /Applications/XAMPP/htdocs/work/svn/Facebook-Auto-Post/run.py
+    echo "Running the script now"
+    python /Applications/XAMPP/htdocs/work/svn/Facebook-Auto-Post/run.py
 
-echo "Deactivating VirtualENV"
-deactivate
+    echo "Deactivating VirtualENV"
+    deactivate
+elif [ $1 = "amazonaffiliates" ];then
+    echo "Activating VirtualENV"
+    . /Applications/XAMPP/htdocs/work/svn/Facebook-Auto-Post/venv/bin/activate
+
+    echo "Running the script now"
+    python /Applications/XAMPP/htdocs/work/svn/Facebook-Auto-Post/AmazonAffiliateFunctions.py
+
+    echo "Deactivating VirtualENV"
+    deactivate
+fi
